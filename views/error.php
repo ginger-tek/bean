1d3b344ec237b2fea8b830976934b85ef7256a51
ginger-tek <jeremy.paul.moormann@gmail.com>
2024-02-12 00:10:13 -0800

    remove internal error exposure

--- views/error.php
+++ views/error.php
@@ -1,2 +1,4 @@
-<h2>An error occurred</h2>
-<?= $msg ?>
+<h2>Whoa. That's not good.</h2>
+<p>Looks like something went wrong. You can try refreshing the page, or head back to the previous one.</p>
+<br>
+<div role="button" class="inline" onclick="history.back()">Go Back</div>
